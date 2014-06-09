dc1d5d4917d5632b4cf3150b73a4fb26e400940f
Matt Pillar <matt@aerofs.com>
2014-06-09 11:36:10 -0700

    Add full CSV download to web interface.

--- linkedin.js
+++ linkedin.js
@@ -3,9 +3,21 @@ users = new Meteor.Collection("users");
 
 if (Meteor.isClient) {
     Template.users.users = function () {
+        console.log(users.find({}, {sort: {date: -1}, limit: 20}));
         return users.find({}, {sort: {date: -1}, limit: 20});
     };
+
     Template.users.count = function () {
         return users.find().count()
+    };
+
+    Template.users.csv = function () {
+        var usersList = [];
+
+        users.find().forEach(function(user) {
+            usersList.push(user.id + ',' + user.name + ',' + user.locality.replace(/,/g, ''));
+        });
+
+        return encodeURIComponent(usersList.join('\n'));
     };
 }
